290cb0f90a40bd81009c1c7ccdac1920197631f8
gevorg kesyan <gkesyan@luxoft.com>
2021-06-30 01:08:59 +0300

    reverse convertion fix

--- app/binance.go
+++ app/binance.go
@@ -21,15 +21,15 @@ func GetCourseUsd(currency string, curseChan chan BinanceResponse) (jsonResponse
     return
 }
 
-func GetUsdConvert(currency string, rubChan chan BinanceResponse) (err error) {
-    defer close(rubChan)
+func GetUsdConvert(currency string, currChan chan BinanceResponse) (err error) {
+    defer close(currChan)
     jsonResponse, err := makeRequest(fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=USDT%s", currency))
     if err != nil {
         jsonResponse, err = makeRequest(fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=%sUSDT", currency))
         jsonResponse.Price = math.Pow(jsonResponse.Price, -1)
     }
 
-    rubChan <- jsonResponse
+    currChan <- jsonResponse
     return
 }
 
